a83d555585cb985d85189e5e09b457c7ac77330f
Roman Remizov <rmremizov@gmail.com>
2023-04-24 20:26:48 +0500

    Update to_toml.py

--- filter_plugins/to_toml.py
+++ filter_plugins/to_toml.py
@@ -6,4 +6,4 @@ class FilterModule(object):
         return {"to_toml": self.to_toml}
 
     def to_toml(self, input):
-        return toml.dumps(input)
+        return toml.dumps(json.loads(json.dumps(input)))
